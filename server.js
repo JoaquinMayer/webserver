1701feb1d0a0b5e7219f3c404125e9270123fc76
Joaquin Mayer <joaquin.mayer@vix.com>
2018-11-30 14:41:25 -0300

    Partials Handlerbars

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const hbs = require('hbs');
+
 const app = express()
 
 app.use(express.static(__dirname + '/public'))
+
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
 
@@ -9,6 +13,13 @@ app.get('/', (req, res) => {
 
     res.render('home', {
         nombre: 'Joaquin',
+        anio: new Date().getFullYear()
+    })
+})
+
+app.get('/about', (req, res) => {
+
+    res.render('about', {
         anio: new Date().getFullYear()
     })
 })
